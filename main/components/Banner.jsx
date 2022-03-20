771908de141ddf5383ac156aa32bdfb471bad2f8
Utkarsh_agrawal <jiren0359san@gmail.com>
2022-03-20 23:07:29 +0530

    Added type writer effect on banner

--- main/components/Banner.jsx
+++ main/components/Banner.jsx
@@ -1,28 +1,26 @@
 import Image from 'next/image'
 import bannerImg from '../assets/banner.svg'
 import Typewriter from 'typewriter-effect';
-import Modal from 'react-modal'
-import Form from './Form';
 import styles from './Banner.module.scss';
 import { useContext } from 'react';
 import { AppContext } from '../../AppContext';
-import withForm from '../../HOC/withForm';
 
 
 const phrases = [
     "Full Stack Web \n development.",
     "Digital Marketing.",
-    "Data Science."
 ]
 
-function Banner() {
+export default function Banner() {
 
     const { setToggleLoginForm, setIsAdmin } = useContext(AppContext)
         
     const handleForm = function () {
         setIsAdmin(false);
         setToggleLoginForm(true);
+    
     }
+
 
     return (
         <section className={styles.container}>
@@ -30,18 +28,17 @@ function Banner() {
                 <Image src={bannerImg} alt="study first pay later" objectFit="contain" layout="responsive"/>
             </figure>
             <div className={styles.banner_info}>
-                <h1 className={styles.title}>study now and pay later</h1>
-                {/* <span className={styles.text}>Courses in</span> */}
-                    {/* <div className={styles.typewriter}>
+                <h1 className={styles.title}>Study now and pay later</h1>
+                    <div className={styles.typewriter}>
                         <Typewriter
                             onInit={t=>t.changeDeleteSpeed(1).changeDelay(0.4)}
                             options={{
                                 strings: phrases,
                                 autoStart: true,
                                 loop: true,
-                                }}
+                            }}
                         />
-                </div> */}
+                </div>
                 <p className={styles.desc}>From Basics to Advance, learn all that is <br/>
                     all about Any Course. Get up-to-date <br/>
                     lectures from our industry experts.</p>
@@ -50,5 +47,3 @@ function Banner() {
         </section>
     )
 }
-
-export default Banner
