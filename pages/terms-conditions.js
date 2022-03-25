dd8d2fdb3f1c990d1b59474f6ef74b449db66331
@haroon11 <haroon@netart.io>
2022-03-25 11:45:45 +0530

    added terms and privacy pages

---
+++ pages/terms-conditions.js
@@ -0,0 +1,200 @@
+import styled from 'styled-components'
+
+const Section = styled.section``
+
+const termsConditions = () => {
+  return (
+    <div style={{ marginTop: '80px' }}>
+      <h1 className='c10' id='h.gjdgxs' style={{ marginBottom: '10px' }}>
+        <span className='c16'>Terms &amp; Conditions</span>
+      </h1>
+      <p className='' style={{ marginBottom: '10px' }}>
+        <span className='c15'>
+          By using this website, Digital Aided School.com
+          (&ldquo;Website&rdquo;), and the services provided by Digital Aided
+          School, you are agreeing to be bound by the following Terms and
+          Conditions (&ldquo;Terms of Service&rdquo;). If you do not agree with
+          the Terms of Service you must not use this Website or any Service
+          provided in it. Digital Aided School reserves the right to update and
+          change the Terms of Service periodically, without notice. Any new
+          feature or service provided by Digital Aided School shall be subject
+          to the Terms of Service. Continued use of the Website and Services
+          shall constitute your consent to the changes of the Terms of Service.
+          Digital Aided School reserves the right to terminate your account or
+          to stop providing Services should you violate any of the terms below.
+        </span>
+      </p>
+      <h3 className='c0' id='h.30j0zll'>
+        <span className='c4'>1. Terms</span>
+      </h3>
+      <p className='c1'>
+        <span className='c7 c8'>
+          By accessing this web site, you are agreeing to be bound by these web
+          site Terms and Conditions of Use, all applicable laws and regulations,
+          and agree that you are responsible for compliance with any applicable
+          local laws. If you do not agree with any of these terms, you are
+          prohibited from using or accessing this site. The materials contained
+          in this web site are protected by applicable copyright and trademark
+          law.
+        </span>
+      </p>
+      <h3 className='c0' id='h.1fob9te'>
+        <span className='c4'>2. Use License</span>
+      </h3>
+      <p className='c1'>
+        <span className='c7 c8'>
+          Permission is granted to temporarily download one copy of the
+          materials (information or software) on Digital Aided School&#39;s
+          website for personal, non-commercial transitory viewing only. This is
+          the grant of a license, not a transfer of title, and under this
+          license you may not:
+        </span>
+      </p>
+      <ul className='c11 lst-kix_list_1-0 start'>
+        <li className='c5 li-bullet-0'>
+          <span className='c2'>modify or copy the materials;</span>
+        </li>
+        <li className='c5 li-bullet-0'>
+          <span className='c2'>
+            use the materials for any commercial purpose, or for any public
+            display (commercial or non-commercial);
+          </span>
+        </li>
+        <li className='c5 li-bullet-0'>
+          <span className='c2'>
+            attempt to decompile or reverse engineer any software contained on
+            Digital Aided School&#39;s website;
+          </span>
+        </li>
+        <li className='c5 li-bullet-0'>
+          <span className='c2'>
+            remove any copyright or other proprietary notations from the
+            materials; or
+          </span>
+        </li>
+        <li className='c13 li-bullet-0'>
+          <span className='c2'>
+            transfer the materials to another person or&quot;mirror&quot; the
+            materials on any other server.
+          </span>
+        </li>
+      </ul>
+      <p className='c1'>
+        <span className='c7 c8'>
+          This license shall automatically terminate if you violate any of these
+          restrictions and may be terminated by Digital Aided School at any
+          time. Upon terminating your viewing of these materials or upon the
+          termination of this license, you must destroy any downloaded materials
+          in your possession whether in electronic or printed format.
+        </span>
+      </p>
+      <h3 className='c0' id='h.3znysh7'>
+        <span className='c4'>3. Disclaimer</span>
+      </h3>
+      <p className='c1'>
+        <span className='c7 c8'>
+          The materials on Digital Aided School&#39;s website are provided
+          &quot;as is&quot;. Digital Aided School makes no warranties, expressed
+          or implied, and hereby disclaims and negates all other warranties,
+          including without limitation, implied warranties or conditions of
+          merchantability, fitness for a particular purpose, or non-infringement
+          of intellectual property or other violation of rights. Further,
+          Digital Aided School. does not warrant or make any representations
+          concerning the accuracy, likely results, or reliability of the use of
+          the materials on its Internet web site or otherwise relating to such
+          materials or on any sites linked to this site. Our program and
+          curriculum may change without any prior notifications. Digital Aided
+          School does not guarantee jobs or referrals. Digital Aided School
+          charges money for skill development and you have to pay us if you get
+          a job thorugh any medium or decide not to do a job. Our instructors
+          and mentors are from various companies and may change anytime. Digital
+          Aided School will not provide you any referrals if you get/deny a job
+          offer from any source.
+        </span>
+      </p>
+      <h3 className='c0' id='h.2et92p0'>
+        <span className='c4'>4. Limitations</span>
+      </h3>
+      <p className='c1'>
+        <span className='c7 c8'>
+          In no event shall Digital Aided School or its suppliers be liable for
+          any damages (including, without limitation, damages for loss of data
+          or profit, or due to business interruption) arising out of the use or
+          inability to use the materials on Digital Aided School&#39;s Internet
+          site, even if Digital Aided School or a Digital Aided School
+          authorized representative has been notified orally or in writing of
+          the possibility of such damage. Because some jurisdictions do not
+          allow limitations on implied warranties, or limitations of liability
+          for consequential or incidental damages, these limitations may not
+          apply to you.
+        </span>
+      </p>
+      <h3 className='c0' id='h.tyjcwt'>
+        <span className='c4'>5. Revisions and Errata</span>
+      </h3>
+      <p className='c1'>
+        <span className='c7 c8'>
+          The materials appearing on Digital Aided School&#39;s web site could
+          include technical, typographical, or photographic errors. Digital
+          Aided School does not warrant that any of the materials on its web
+          site are accurate, complete, or current. Digital Aided School may make
+          changes to the materials contained on its web site at any time without
+          notice. Digital Aided School does not, however, make any commitment to
+          update the materials.
+        </span>
+      </p>
+      <h3 className='c0' id='h.3dy6vkm'>
+        <span className='c4'>6. Links</span>
+      </h3>
+      <p className='c1'>
+        <span className='c7 c8'>
+          Digital Aided School has not reviewed all of the sites linked to its
+          Internet web site and is not responsible for the contents of any such
+          linked-site. The inclusion of any link does not imply endorsement by
+          Digital Aided School of the site. Use of any such linked web site is
+          at the user&#39;s own risk.
+        </span>
+      </p>
+      <h3 className='c0' id='h.1t3h5sf'>
+        <span className='c4'>7. Site Terms of Use Modifications</span>
+      </h3>
+      <p className='c1'>
+        <span className='c7 c8'>
+          Digital Aided School may revise these terms of use for its web site at
+          any time without notice. By using this web site you are agreeing to be
+          bound by the then current version of these Terms and Conditions of
+          Use.
+        </span>
+      </p>
+      <h3 className='c0' id='h.4d34og8'>
+        <span className='c4'>8. Permitted Use</span>
+      </h3>
+      <p className='c1'>
+        <span className='c7 c8'>
+          Digital Aided School IS PROVIDED &quot;AS IS&quot;,WITH NO WARRANTIES
+          WHATSOEVER. Digital Aided School DISCLAIMS ANY WARRANTIES REGARDING
+          THE SECURITY, RELIABILITY, TIMELINESS, AND PERFORMANCE OF THE SERVICE.
+        </span>
+      </p>
+      <h3 className='c0' id='h.2s8eyo1'>
+        <span className='c4'>9. Indemnity</span>
+      </h3>
+      <p className='c1'>
+        <span className='c7 c8'>
+          You will indemnify and hold us, our suppliers and licensors, and our
+          respective subsidiaries, affiliates, officers, agents, employees,
+          representatives, and assigns harmless from any costs, damages,
+          expenses, and liability caused by your use of the Services and Service
+          Content, your violation of these Terms, or your violation of any
+          rights of a third party through use of the Services or Service
+          Content.
+        </span>
+      </p>
+      <p className='c12'>
+        <span className='c3'></span>
+      </p>
+    </div>
+  )
+}
+
+export default termsConditions
