534524e1d91081c49ef11364605073305c559b7f
vikasmourya <60725905+thisisvikasmourya@users.noreply.github.com>
2022-10-18 09:07:58 +0530

    error-fix-18/10/22

--- pages/Dashboard/index.js
+++ pages/Dashboard/index.js
@@ -1,33 +1,33 @@
-// import React, { useContext } from 'react'
-// import {useRouter} from 'next/router'
-// import { fetchLead } from '../../main/action/action'
-// import { toast } from 'react-toastify'
-// import { AppContext } from '../../AppContext'
+import React, { useContext } from 'react'
+import {useRouter} from 'next/router'
+import { fetchLead } from '../../main/action/action'
+import { toast } from 'react-toastify'
+import { AppContext } from '../../AppContext'
 
-// function Dashboard() {
+function Dashboard() {
 
-//     const router = useRouter()
+    const router = useRouter()
 
-//     const {dispatch, state : { leads : {data}, admin : {token} }} = useContext(AppContext)
+    const {dispatch, state : { leads : {data}, admin : {token} }} = useContext(AppContext)
     
 
-//     const handleShowLeads = async () => {
+    const handleShowLeads = async () => {
         
-//         if (!data?.length) {
-//             await fetchLead(dispatch, token, toast)
-//         }
-//         router.push('/Dashboard/leads')
-//     }
+        if (!data?.length) {
+            await fetchLead(dispatch, token, toast)
+        }
+        router.push('/Dashboard/leads')
+    }
     
-//     return (
-//       <>
-//             <button  onClick={handleShowLeads} >Show Leads</button>
-//             <button className="button" onClick={()=> router.push('/')}>Back</button>
-//       </>
-//   )
-// }
+    return (
+      <>
+            <button  onClick={handleShowLeads} >Show Leads</button>
+            <button className="button" onClick={()=> router.push('/')}>Back</button>
+      </>
+  )
+}
 
 
-// Dashboard.withoutLayout = true;
+Dashboard.withoutLayout = true;
 
-// export default Dashboard
+export default Dashboard
