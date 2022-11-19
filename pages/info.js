7b5db74137902264f63d19df16b7a2a2d253205d
Hamid <www.abdulhamid3311@gmail.com>
2022-11-19 15:24:20 +0600

    feat:login,leadForm

---
+++ pages/info.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Dashboar from '../main/components/Dashboard/Dashboar';
+
+
+const info = () => {
+    return (
+        <div className='leads-info-container' style={{ textAlign: "center", color: "black" }}>
+            <Dashboar />
+        </div>
+    );
+};
+
+export default info;
