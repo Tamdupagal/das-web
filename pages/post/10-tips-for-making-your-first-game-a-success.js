e77303594b37efeb11b4c6afc19391c5cb4bb050
vikasmourya <60725905+thisisvikasmourya@users.noreply.github.com>
2022-12-09 01:42:25 +0530

    blogadd

--- pages/post/10-tips-for-making-your-first-game-a-success.js
+++ pages/post/10-tips-for-making-your-first-game-a-success.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import Image from 'next/image'
+import intro from '../../assets/post-images/tip.png'
+import first from '../../assets/post-images/tipfr.png'
+import second from '../../assets/post-images/tipsec.png'
+import third from '../../assets/post-images/tipth.png'
+import four from '../../assets/post-images/tipthank.png'
+
 
 const Post2 = () => {
   return (
     <div>
      <div style={{paddingTop:'10rem'}} className="container2">
     <h1 className="title">10 tips for making your first game a success</h1>
+    <div className='imgg'> 
+    <Image src={intro} alt='intro'  className='imgg' />
+    </div>
     <p style={{fontSize:'15px'}}>Date: 05/12/2022  / 12:00Pm</p>
     <div className='imgg'> 
     </div>
@@ -24,6 +33,9 @@ const Post2 = () => {
       Write down as much information as possible about what it is about and why others need to know about it. The more detail, the better!
       Write in a simple style that makes sense for where you are in the process (i.e., no fancy words or big blocks). If something seems too long-winded, write short sentences instead of paragraphs—this will help keep things moving faster!
       </p>
+      <div className='imgg'> 
+    <Image src={first} alt='intro'  className='imgg' />
+    </div>
       <h2 className="sub-heading">Create a storyline. Your storyline will act as the foundation of your game.</h2>
       <p style={{paddingTop:'1rem',}} className="description">
       Your storyline will act as the foundation of your game. Before starting development, You must ensure that you have a strong story. A good story should be interesting, engaging for players, and also relatable to them—so it must be easy for people who don&apos;t play games every day (like non-gamers) to understand what&apos; going on.
@@ -41,6 +53,9 @@ The most important part of character creation is that they should be relatable. 
 When creating a new character in your game, ask yourself: Do I like this person? Why do I like them? What makes it so special? If a character isn&pos;t relatable or interesting, then there&pos;s no point in making it into one. Because nobody will want to play as them after seeing how awkward they look on the screen with their teeth pointing out at strange angles and their hair standing straight up like someone just came through there with a spray bottle full off hairspray trying to fix their frizz problem.
 
 </p>
+<div className='imgg'> 
+    <Image src={second} alt='intro'  className='imgg' />
+    </div>
 <h2 className="sub-heading">Have the main character that can relate to the audience.</h2>
 <p style={{paddingTop:'1rem',}} className="description">
 It is important to have a main character that can relate to the audience. The best way to do this is by ensuring your main character is interesting, relatable and someone people want to play or be.
@@ -64,6 +79,10 @@ When choosing music for your game:
 <li>Make sure the game isn&pos;t too short or long.</li>
 <li>Make sure the game doesn&pos;t have any special rules that would make it difficult for a player to understand what&pos;s going on in each level (for example, if you&pos;re playing and your mouse is stuck in the middle of an obstacle course).</li>
 </div>
+
+<div className='imgg'> 
+    <Image src={third} alt='intro'  className='imgg' />
+    </div>
 <h2 className="sub-heading">Ensure that you have enough content in your game before releasing it to the public.</h2>
 <p style={{paddingTop:'1rem',}} className="description">
 You must have enough content before releasing your game. The evaluation of your game will depend on how much it offers to the players, so you must make sure they can access all of the information they need and learn from your game.
@@ -79,10 +98,15 @@ Rushing the development process can be disastrous for your game. Take your time,
 <p style={{paddingTop:'1rem',}} className="description">
 Making your first game is intimidating, but with these tips, you&pos;re bound for success!
 </p>
+
 <div className='list'>
 <li>Don&pos;t be intimidated. You can do it! If you&pos;ve got a good idea and the skills to create it, then this is an opportunity for growth. The most important thing is to be confident in yourself—don&pos;t let anyone else&pos;s opinion influence yours (even if they are people who worked on games before).</li>
 <li>Set goals, and don&pos;t give up. It&pos;s easy to get discouraged by setbacks or other obstacles along the way—but try not to let that stop you from reaching your goal! If something doesn&pos;t go as planned at first glance (like losing all of your progress), don&pos;t give up. Figure out how best to handle those situations so they won&pos;t happen again later down the line when building something new again later on down the road after creating several successful titles over time.</li>
 </div>
+
+<div className='imgg'> 
+    <Image src={four} alt='intro'  className='imgg' />
+    </div>
 <h2 className="sub-heading">Conclusion</h2>
 <p style={{paddingTop:'1rem',}} className="description">
 You’re on the right track now, and we hope you enjoyed reading this post. We know how intimidating it can be to start your game development career or make games on your own. If you need help, don’t hesitate to reach out! We at Digital Aided School provide live online full-stack game development courses. We are an online learning platform that provides training and placement support to creators and helps them succeed in their game development journey.
