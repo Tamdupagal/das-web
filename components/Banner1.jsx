53658a9df9a94ae10a9e11cc9eca9955badb8f41
vikasmourya <60725905+thisisvikasmourya@users.noreply.github.com>
2022-12-23 21:54:49 +0530

    doneeee

--- components/Banner1.jsx
+++ components/Banner1.jsx
@@ -1,42 +1,42 @@
-import { useContext } from 'react'
-import React from 'react'
-import styles from './Banner.module.scss'
-import { GoPrimitiveDot as Dot } from 'react-icons/go'
-import { AppContext } from '../AppContext'
-import Link from 'next/link'
-import Popup from 'reactjs-popup'
-import LeadForm from '../main/components/Form/LeadForm'
+import { useContext } from "react";
+import React from "react";
+import styles from "./Banner.module.scss";
+import { GoPrimitiveDot as Dot } from "react-icons/go";
+import { AppContext } from "../AppContext";
+import Link from "next/link";
+import Popup from "reactjs-popup";
+import LeadForm from "../main/components/Form/LeadForm";
 
 const FadeIn = {
   initial: {
-    y: '5rem',
+    y: "5rem",
     opacity: 0,
   },
   vissible: {
     y: 0,
     opacity: 1,
     transition: {
-      type: 'spring',
+      type: "spring",
       bouces: 0.25,
       delay: 0.2,
     },
   },
-}
+};
 
 function Banner({ data, checkBanner }) {
-  const { setToggleLoginForm, setIsAdmin } = useContext(AppContext)
+  const { setToggleLoginForm, setIsAdmin } = useContext(AppContext);
 
   const handleForm = function () {
-    setIsAdmin(false)
-    setToggleLoginForm(true)
-  }
+    setIsAdmin(false);
+    setToggleLoginForm(true);
+  };
 
   return (
-    <section className={styles.container} id='home'>
+    <section className={styles.container} id="home">
       <div
         variants={FadeIn}
-        animate='vissible'
-        initial='initial'
+        animate="vissible"
+        initial="initial"
         className={styles.content__box}
       >
         <h1 className={styles.heading}>Learn First, Pay Later !</h1>
@@ -46,14 +46,15 @@ function Banner({ data, checkBanner }) {
           <ul>
             <div clasName={styles.dev}>
               <li>
-                <Dot className={styles.dot} /> Live online classes{' '}
+                <Dot className={styles.dot} /> Live online classes{" "}
               </li>
               <li>
-                <Dot className={styles.dot} /> Agency style cohort-based learning{' '}
+                <Dot className={styles.dot} /> Agency style cohort-based
+                learning{" "}
               </li>
               <li>
                 <Dot className={styles.dot} /> 1:10 collaborative Live project
-                building academic experience{' '}
+                building academic experience{" "}
               </li>
             </div>
             <div clasName={styles.dev}>
@@ -72,36 +73,33 @@ function Banner({ data, checkBanner }) {
           </ul>
           <span>Batches</span>
         </div>
-        <p style={{ textAlign: 'center', fontSize: '16px', color: '#005778' }}>
+        <p style={{ textAlign: "center", fontSize: "16px", color: "#005778" }}>
           The Next batch starts from 1 December 2022
         </p>
         <button className={styles.banner__btn} onClick={handleForm}>
-
           <p className={styles.Link}>
             <Popup
-              trigger={<a target="_blank" className={styles.Link}>PAY AS YOU LIKE</a>}
-              position="center">
-              {
-                close => (
-                  <LeadForm close={close} />
-                )
+              trigger={
+                <a target="_blank" className={styles.Link}>
+                  PAY AS YOU LIKE
+                </a>
               }
-
+              position="center"
+            >
+              {(close) => <LeadForm close={close} />}
             </Popup>
           </p>
-
         </button>
         <p className={styles.regis}>Registration Charge</p>
-        <Link href="https://rzp.io/l/xVAzXSaY6" passHref={true}>
-          <div className={styles.price} style={{ marginTop: '12px', padding: '8px', borderRadius: '8px' }}>
-            <b style={{ color: '#fff' }}>
-              Was  <s>₹10000</s> &nbsp;Now ₹2999 + GST
-            </b>
-          </div>
-        </Link>
+        <div
+          className={styles.price}
+          style={{ marginTop: "12px", padding: "8px", borderRadius: "8px" }}
+        >
+          <b style={{ color: "#fff" }}>&nbsp; ₹30000 + GST</b>
+        </div>
       </div>
     </section>
-  )
+  );
 }
 
-export default Banner
+export default Banner;
--- main/components/Banner.jsx
+++ main/components/Banner.jsx
@@ -1,73 +1,66 @@
-import Typewriter from 'typewriter-effect';
-import styles from './Banner.module.scss';
-import { useContext } from 'react';
-import { AppContext } from '../../AppContext';
-import Link from 'next/link'
-import Popup from 'reactjs-popup';
-import LeadForm from './Form/LeadForm';
+import Typewriter from "typewriter-effect";
+import styles from "./Banner.module.scss";
+import { useContext } from "react";
+import { AppContext } from "../../AppContext";
+import Link from "next/link";
+import Popup from "reactjs-popup";
+import LeadForm from "./Form/LeadForm";
 
-const phrases = [
-    "Game Development.",
-    "Web3 Courses.",
-]
+const phrases = ["Game Development.", "Web3 Courses."];
 
 export default function Banner() {
+  const { setToggleLoginForm, setIsAdmin } = useContext(AppContext);
 
-    const { setToggleLoginForm, setIsAdmin } = useContext(AppContext)
-
-    /*  const handleForm = function () {
+  /*  const handleForm = function () {
          setIsAdmin(false);
          setToggleLoginForm(true);
      } */
 
+  return (
+    <section className={styles.container}>
+      <div className={styles.banner_info}>
+        <h1 className={styles.title}>Study Now and Pay later</h1>
+        <div className={styles.typewriter}>
+          <Typewriter
+            onInit={(t) => t.changeDeleteSpeed(1).changeDelay(0.4)}
+            options={{
+              strings: phrases,
+              autoStart: true,
+              loop: true,
+            }}
+          />
+        </div>
+        <div className={styles.para}>
+          <h1 className={styles.title2}>
+            Be a Fullstack Game Developer As Per Industry Standards
+            <br /> Portfolio & Interview Ready Program.
+          </h1>
+          <h1 className={styles.title3}>
+            Connect with the real gaming community.
+          </h1>
+          <h1 className={styles.title3}>Be a Creator, Not a Player</h1>
+        </div>
+        <button className={styles.button}>
+          <Popup
+            trigger={
+              <a target="_blank" className={styles.Link}>
+                Study now and pay later
+              </a>
+            }
+            position="center"
+          >
+            {(close) => <LeadForm close={close} />}
+          </Popup>
+        </button>
 
-    return (
-        <section className={styles.container}>
-
-            <div className={styles.banner_info}>
-                <h1 className={styles.title}>Study Now and Pay later</h1>
-                <div className={styles.typewriter}>
-                    <Typewriter
-                        onInit={t => t.changeDeleteSpeed(1).changeDelay(0.4)}
-                        options={{
-                            strings: phrases,
-                            autoStart: true,
-                            loop: true,
-                        }}
-                    />
-                </div>
-                <div className={styles.para}>
-                    <h1 className={styles.title2}>
-                        Be a Fullstack Game Developer As Per Industry Standards<br /> Portfolio & Interview Ready Program.
-                    </h1>
-                    <h1 className={styles.title3}>Connect with the real gaming community.</h1>
-                    <h1 className={styles.title3}>Be a Creator, Not a Player</h1>
-                </div>
-                <button className={styles.button}>
-                    <Popup
-                        trigger={<a target="_blank" className={styles.Link}>Study now and pay later</a>}
-                        position="center">
-                        {
-                            close => (
-                                <LeadForm close={close} />
-                            )
-                        }
-
-                    </Popup>
-                </button>
-
-                <p className={styles.regis}>Registration Charge</p>
-
-                <Link href="https://rzp.io/l/xVAzXSaY6" passHref={true}>
-                    <div className={styles.price} style={{ marginTop: '12px', padding: '8px', borderRadius: '8px' }}>
-                        <b style={{ color: '#fff' }}>
-                            Was  <s>₹10000</s> &nbsp;Now ₹2999 + GST
-                        </b>
-                    </div>
-                </Link>
-
-            </div>
-        </section>
-    )
+        <p className={styles.regis}>Registration Charge</p>
+        <div
+          className={styles.price}
+          style={{ marginTop: "12px", padding: "8px", borderRadius: "8px" }}
+        >
+          <b style={{ color: "#fff" }}>₹30000 + GST</b>
+        </div>
+      </div>
+    </section>
+  );
 }
-
