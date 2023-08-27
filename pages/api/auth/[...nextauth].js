e26b9bb1bd8ab794c71cb3dd444d2d61ca794940
vikasmourya <60725905+thisisvikasmourya@users.noreply.github.com>
2023-08-28 00:43:20 +0530

    done

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,29 +1,29 @@
-import nextAuth, { NextAuthOptions } from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
+// import nextAuth, { NextAuthOptions } from "next-auth";
+// import CredentialsProvider from "next-auth/providers/credentials";
 
-export const authOptions = {
-    session: {
-        strategy: "jwt"
-    },
-    providers: [
-        CredentialsProvider({
-            type: "credentials",
-            credentials: {},
-            authorize(credentials, req) {
-                const { email, password } = credentials;
-                //perform other logics
-                //find users from DB
-                if (email !== "admin@digitalaidedschool.com" || password !== "DAS@admin123") {
-                    throw new Error('invalid credentials')
-                    return null
-                }
+// export const authOptions = {
+//     session: {
+//         strategy: "jwt"
+//     },
+//     providers: [
+//         CredentialsProvider({
+//             type: "credentials",
+//             credentials: {},
+//             authorize(credentials, req) {
+//                 const { email, password } = credentials;
+//                 //perform other logics
+//                 //find users from DB
+//                 if (email !== "admin@digitalaidedschool.com" || password !== "DAS@admin123") {
+//                     throw new Error('invalid credentials')
+//                     return null
+//                 }
 
-                //if all fine return
-                return { "id": "201", "name": "Admin", "email": "admin@digitalaidedschool.com" }
-            }
-        })
-    ],
-    secret: process.env.NEXTAUTH_SECRET,
-}
+//                 //if all fine return
+//                 return { "id": "201", "name": "Admin", "email": "admin@digitalaidedschool.com" }
+//             }
+//         })
+//     ],
+//     secret: process.env.NEXTAUTH_SECRET,
+// }
 
-export default nextAuth(authOptions)
+// export default nextAuth(authOptions)
